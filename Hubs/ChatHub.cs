c05e0a35b07898a75807ae005bd3f9baa2a78345
Emmanuel Addo Odame <eaodame@hubtel.com>
2021-06-04 08:35:58 +0000

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApi.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task AddToGroup(string groupName, string user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Entered", user);
+        }
+
+        public async Task RemoveFromGroup(string groupName, string user)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Left", user);
+        }
+
+        public async Task SendMessageGroup(string groupName, string user, string message)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+
+}
